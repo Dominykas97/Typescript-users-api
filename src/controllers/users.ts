e18331959d517697a0405b1984e22ce3ada0e38c
Dominykas Senkus <dominykas97@gmail.com>
2023-01-02 20:12:18 +0000

    create users in db

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,61 +1,56 @@
 import { RequestHandler } from "express";
 import { Admin, Employee, PowerUser, User, USER_TYPE } from '../models/users';
 import { db } from "../db";
-import { OkPacket, ResultSetHeader, RowDataPacket } from "mysql2";
+import { ResultSetHeader } from "mysql2";
 const USERS: User[] = [];
 
-
-export const createUser: RequestHandler = (req: { body: User }, res) => {
+export const createUser: RequestHandler = (req: { body: User }, res, next) => {
     const name = req.body.name;
     const age = req.body.age;
     const type = req.body.type;
-    let newUser: User | undefined;
-    if (name && age && type) {
-        if (typeof (name) !== "string") {
-            return res.status(400).json({ err: 'name is not a string' });
-        }
-        if (typeof (age) !== "number") {
-            return res.status(400).json({ err: 'age is not a number' });
-        }
-        if (!Object.values(USER_TYPE).includes(type as USER_TYPE)) {
-            const availableTypesNoQuotes = JSON.parse(JSON.stringify(Object.values(USER_TYPE)).replace(/&quot;/g, '"'));
-            return res.status(400).json({ err: `type is invalid. Available types: ${availableTypesNoQuotes}` });
-        }
-        if (type === USER_TYPE.ADMIN) {
-            const role = (req.body as Admin).role;
-            if (role) {
-                newUser = new Admin(name, age, type, role);
-            } else {
-                return res.status(400).json({ err: 'role is missing for admin' });
-            }
-        }
-        if (type === USER_TYPE.EMPLOYEE) {
-            const occupation = (req.body as Employee).occupation;
-            if (occupation) {
-                newUser = new Admin(name, age, type, occupation);
-            } else {
-                return res.status(400).json({ err: 'occupation is missing for employee' });
-            }
-        }
-        if (type === USER_TYPE.POWERUSER) {
-            const role = (req.body as PowerUser).role;
-            const occupation = (req.body as PowerUser).occupation;
-            if (role && occupation) {
-                newUser = new PowerUser(name, age, type, role, occupation);
-            } else {
-                return res.status(400).json({ err: 'role and/or occupation is missing for poweruser' }).end();
-            }
-        }
-
-        if (newUser) {
-            USERS.push(newUser);
-            return res.status(200).json({ message: 'Created a new user', createdUser: newUser });
-        } else {
-            return res.status(500).json({ err: 'Could not create a new user' });
-        }
-    } else {
+    let role, occupation;
+    if (!(name && age && type)) {
         return res.status(400).json({ err: 'Could not create a new user: name, age, and/or type missing' });
     }
+    if (typeof (name) !== "string") {
+        return res.status(400).json({ err: 'name is not a string' });
+    }
+    if (typeof (age) !== "number") {
+        return res.status(400).json({ err: 'age is not a number' });
+    }
+    if (!Object.values(USER_TYPE).includes(type as USER_TYPE)) {
+        const availableTypesNoQuotes = JSON.parse(JSON.stringify(Object.values(USER_TYPE)).replace(/&quot;/g, '"'));
+        return res.status(400).json({ err: `type is invalid. Available types: ${availableTypesNoQuotes}` });
+    }
+    else if (type === USER_TYPE.ADMIN) {
+        role = (req.body as Admin).role;
+        if (!role) {
+            return res.status(400).json({ err: 'role is missing for admin' });
+        }
+    }
+    else if (type === USER_TYPE.EMPLOYEE) {
+        occupation = (req.body as Employee).occupation;
+        if (!occupation) {
+            return res.status(400).json({ err: 'occupation is missing for employee' });
+        }
+    }
+    else if (type === USER_TYPE.POWERUSER) {
+        role = (req.body as PowerUser).role;
+        occupation = (req.body as PowerUser).occupation;
+        if (!(role && occupation)) {
+            return res.status(400).json({ err: 'role and/or occupation is missing for poweruser' }).end();
+        }
+    }
+    const queryString = `INSERT INTO users VALUES (null, "${name}", ${age}, "${type}", "${role ?? "null"}", "${occupation ?? "null"}")`;
+    console.log(queryString)
+    db.query(queryString, (err, result: ResultSetHeader) => {
+        console.log(result);
+        if (err) { return next({ err: err.message }); }
+        if (result.affectedRows < 1) {
+            return res.status(400).json({ err: 'Could not create a new user' });
+        }
+        return res.status(200).json({ message: `Created a new user with id: ${result.insertId}` });
+    });
 };
 
 
