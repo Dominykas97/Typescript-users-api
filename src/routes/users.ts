164fb8e4100571b1217e1ae031d267f1b160e072
Dominykas Senkus <dominykas97@gmail.com>
2023-01-02 20:45:25 +0000

    get by id works again

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,8 +2,8 @@ import { Router } from "express";
 import { createUser, getUsers, getUser, deleteUser } from "../controllers/users";
 
 const router = Router()
-router.get('/:age?/:type?/:role?/:occupation?', getUsers);
 router.get('/:id', getUser);
+router.get('/:age?/:type?/:role?/:occupation?', getUsers);
 router.post('/', createUser);
 router.delete('/:id', deleteUser);
 
