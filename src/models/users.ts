a8eb1e17f91e8d466c1796217f81d5af3b5107aa
Dominykas Senkus <dominykas97@gmail.com>
2023-01-16 12:48:41 +0000

    Removed unused classes

--- src/models/users.ts
+++ src/models/users.ts
@@ -28,28 +28,3 @@ export interface poweruser {
 }
 
 export type user = admin | employee | poweruser;
-
-class BaseUser {
-    id: string;
-    constructor(public name: string, public age: number, public type: USER_TYPE) {
-        this.id = Math.random().toString(36).slice(2, 7)
-    }
-}
-
-export class Admin extends BaseUser {
-    constructor(name: string, age: number, type: USER_TYPE, public role: string) {
-        super(name, age, type);
-    }
-}
-export class Employee extends BaseUser {
-    constructor(name: string, age: number, type: USER_TYPE, public occupation: string) {
-        super(name, age, type);
-    }
-}
-export class PowerUser extends BaseUser {
-    constructor(name: string, age: number, type: USER_TYPE, public role: string, public occupation: string) {
-        super(name, age, type);
-    }
-}
-
-export type User = Admin | Employee | PowerUser;
